2a47d86e6aebb61cb84a0cca5d4da81f2fc87d5a
Lee <326656669@qq.com>
2020-05-11 15:16:43 +0800

    style & feat : 修改代码格式 并 添加例子

--- tsdemo3/1.ts
+++ tsdemo3/1.ts
@@ -2,14 +2,10 @@
     // null undefined 是所有类型的子类型 即 null undefined 可以赋值给其他所有类型
     let a: null = null
     let b: undefined = undefined
-
     let c: boolean = true
-
     let d: string = 'hi'
     let e: number = 1.223
-
     let obj: object = {}
-
     let n: any = 1
 
     // 枚举 如果不给枚举对象的属性赋值,则会默认为0,1,2递增来代表各个属性
@@ -23,9 +19,7 @@
     let gender2: Gender = Gender.Woman
 
     // 接受参数是任意类型 并且函数没有返回值
-    function print(x: any): void {
-        console.log(x)
-    }
+    function print(x: any): void {console.log(x)}
 
     // 类型转换
     let x: number = 123
@@ -42,21 +36,15 @@
         age: number
         shape: Shape
         hobbits?: Array<string>
-
         say(word: string): void
     }
 
     let lzy: Human = {
         name: 'lzy',
         age: 10,
-        shape: {
-            body: 'O',
-            head: 'U'
-        },
+        shape: {body: 'O', head: 'U'},
         hobbits: ['xxx', 'yyy'],
-        say(word: string) {
-            console.log(`${this.name} : ${word}`);
-        }
+        say(word: string) {console.log(`${this.name} : ${word}`);}
     }
 
     lzy.say('hi ')
@@ -65,13 +53,10 @@
     interface RectConfig {
         width?: number
         height?: number
-
         [propName: string]: number
     }
 
-    function createRect(config: RectConfig): void {
-
-    }
+    function createRect(config: RectConfig): void {}
 
     let config = {
         width: 100,
@@ -82,20 +67,44 @@
 
     interface SecondOperation {
         (a: number, b: number): number
-
         inverseOperation(a: number, b: number): number
     }
 
     let fn: SecondOperation = ((): SecondOperation => {
-        let x: any = function (a: number, b: number): number {
-            return a + b
-        }
-
-        x.inverseOperation = function (a: number, b: number): number {
-            return a - b
-        }
+        let x: any = function (a: number, b: number): number {return a + b}
+        x.inverseOperation = function (a: number, b: number): number {return a - b}
         return x
     })()
 
-    console.log(fn(1, 2))
+    interface Animal {
+        move(): void
+    }
+
+    interface Human1 extends Animal {
+        name: string
+        age: number
+    }
+
+    let lzy1: Human1 = {
+        age: 18,
+        name: 'xxx',
+        move() {
+        }
+    }
+
+    // 声明一个接口 A
+    // 属性 num，类型为 number
+    // 属性 b，类型为布尔
+    // 属性 str，类型为字符串
+    // 属性 optional，类型为字符串，但是此属性是可选的
+    // 属性 add，是一个函数，接收的参数有 n1 和 n2，n1 是一个数字，n2 也是一个数字，返回值是一个数字
+    // 属性 r，类型为字符串，但是此属性是只读的
+    interface A {
+        num: number
+        b: boolean
+        str: string
+        optional?: string
+        readonly r: string
+        (n1: number, n2: number): number
+    }
 }
