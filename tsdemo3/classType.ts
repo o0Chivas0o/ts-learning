907532acddb231b6d00f5c59abc1c8e503962fa0
Lee <326656669@qq.com>
2020-05-11 23:44:25 +0800

    feat : 自己构造一个类

--- tsdemo3/classType.ts
+++ tsdemo3/classType.ts
@@ -48,4 +48,40 @@
     lzy.age = -1
 
     console.log(JSON.stringify(lzy));
+
+    //     声明一个类 A
+    //     声明一个类 B
+    //     B 继承 A（B extends A）
+    //     A 的对象属性有
+    //     a1，类型为 number
+    //     a2，是一个函数，函数的实现随意
+    //     A 的构造函数会初始化 a1
+    //     B 的对象属性有
+    //     b1，类型为 string
+    //     b2，是一个函数，函数的实现随意
+    //     B 的构造函数会初始化 b1
+
+    class A {
+        a1: number
+        a2(): void {
+            console.log(1)
+        }
+        constructor(a1: number) {
+            this.a1 = a1
+        }
+    }
+
+    class B extends A {
+        b1: string
+        b2(): void {
+            console.log(2)
+        }
+        constructor(b1: string, argv: number) {
+            super(argv)
+            this.b1 = b1
+        }
+    }
+
+    let b = new B('xxx', 123)
+    console.log(b);
 }
