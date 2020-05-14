875a54c4c884e166fc17c4b61e319b0d39bdc08d
Lee <326656669@qq.com>
2020-05-14 22:13:53 +0800

    feat : 完成一个简易的 todolist

--- ts_x_vue/src/App.vue
+++ ts_x_vue/src/App.vue
@@ -1,29 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Lzy></Lzy>
+    <div class="inner">
+      <newTodo @addTodo="addTodo"></newTodo>
+      <TodoList :list="list"></TodoList>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import Lzy from './components/lzy.vue';
+    import {Component, Vue} from "vue-property-decorator";
+
+    // import {Component, Vue,Mixins} from "vue-property-decorator";
+    // import {MyMixin} from "./mixins/mixin-one";
+
+    import NewTodo from "./components/newTodo.vue";
+    import TodoList from "./components/todoList.vue";
+
+    interface Todo {
+        name: string;
+        status: 'done' | 'undone' | 'deleted';
+    }
 
     @Component({
         components: {
-            Lzy
+            NewTodo, TodoList
         }
     })
-    export default class App extends Vue {}
+    export default class App extends Vue {
+        list: Array<Todo> = [
+            {name: 'mission-1', status: 'done'},
+            {name: 'mission-2', status: 'done'}
+        ]
+
+        addTodo(name: string) {
+            const todo: Todo = {name, status: 'undone'}
+            this.list.push(todo)
+            console.log(name);
+        }
+    }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    display: flex;justify-content: center;align-items: center;
+    .inner {
+      padding: 20px;border: 1px solid;
+    }
   }
 </style>
---
+++ ts_x_vue/src/components/newTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="text">
+    <button @click="addTodo">添加</button>
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+
+    export default class NewTodo extends Vue {
+        text = ''
+
+        addTodo(): void {
+            this.$emit('addTodo', this.text)
+            this.text = ''
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
